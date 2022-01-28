c1f836c8d6c45f6ffc295d8417b95592f014957c
ghaleprachan <ghale.prachan@gmail.com>
2022-01-28 21:03:47 +0545

    [Upgrade] upgrades swagger version

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,8 +24,8 @@ dependencies {
     runtimeOnly("com.h2database:h2")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
 
-    implementation("io.springfox:springfox-swagger2:2.7.0")
-    implementation("io.springfox:springfox-swagger-ui:2.7.0")
+    implementation("io.springfox:springfox-swagger2:3.0.0")
+    implementation("io.springfox:springfox-swagger-ui:3.0.0")
     /*implementation("io.springfox:springfox-swagger2:3.0.0")
     implementation("io.springfox:springfox-swagger-ui:3.0.0")*/
 
